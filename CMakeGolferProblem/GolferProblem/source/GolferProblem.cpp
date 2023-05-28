9fff7a418cab3e737303286e51131af24c7d4331
HhMorgan <heshammse@gmail.com>
2023-05-28 15:46:58 +0200

    Initialized the workspace

---
+++ CMakeGolferProblem/GolferProblem/source/GolferProblem.cpp
@@ -0,0 +1,12 @@
+﻿// GolferProblem.cpp : Defines the entry point for the application.
+//
+
+#include "../include/GolferProblem.h"
+
+using namespace std;
+
+//int main()
+//{
+//	std::cout << "Hello CMake." << endl;
+//	return 0;
+//}
